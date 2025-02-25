d61b81c0684607072d0084123593f1293e6a8f0e
agathiyan <agathiyanjana@gmail.com>
2025-02-26 00:08:16 +0530

    commited

--- src/main/java/com/bankmgmt/app/rest/BankController.java
+++ src/main/java/com/bankmgmt/app/rest/BankController.java
@@ -2,38 +2,82 @@ package com.bankmgmt.app.rest;
 
 import com.bankmgmt.app.entity.Account;
 import com.bankmgmt.app.service.BankService;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 // TODO: Make this class a Rest Controller
+@RestController
+@RequestMapping("accounts")
 public class BankController {
 
     // TODO Autowired the BankService class
+	@Autowired
     private BankService bankService;
 
     // TODO: API to Create a new account
+    @PostMapping("")
+    public Account addAccount(@RequestBody Account account) {
+    	System.out.println("Inside addaccountcontroller");
+    	return bankService.addAccount(account);
+    }
     
 
     // TODO: API to Get all accounts
+    @GetMapping("")
+    public List<Account> getAllAccounts() {
+    	return bankService.getAllAccounts();
+    }
     
 
     // TODO: API to Get an account by ID
+    @GetMapping("/{id}")
+    public Account getAccountById(@PathVariable ("id") int id) {
+    	System.out.println("inside by id");
+    	return bankService.getAccountbyId(id);
+    }
     
 
     // TODO: API to Deposit money
+    @PostMapping("/{id}/deposit")
+    public Account deposit(@PathVariable ("id") int id,@RequestBody double amount) {
+    	System.out.println("Inside deposit controller");
+    	return bankService.deposit(id, amount);
+    }
+    
     
 
     // TODO: API to Withdraw money
+    @PostMapping("/{id}/withdraw")
+    public Account withdraw(@PathVariable ("id") int id,double amount) {
+    	System.out.println("inside withdraw controller");
+    	return bankService.withdraw(id, amount);
+    }
     
 
     // TODO: API to Transfer money between accounts
-    
+    @PostMapping("/transfer")
+    public List<Account> transfer(@RequestParam ("fromId") int fromid,@RequestParam("toId") int toid,@RequestParam ("amount") double amount){
+    	System.out.println("inside transfer controller");
+    	return bankService.transferFunds(fromid, toid, amount);
+    }
 
     // TODO: API to Delete an account
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable ("id") int id) {
+    	Account a = bankService.deleteAccount(id);
+    	if (a==null) {
+    		System.out.println("inside null controller");
+    		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+    	}
+    	return null;
+    }
     
 }
--- src/main/java/com/bankmgmt/app/service/BankService.java
+++ src/main/java/com/bankmgmt/app/service/BankService.java
@@ -1,36 +1,145 @@
 package com.bankmgmt.app.service;
 
-import com.bankmgmt.app.entity.Account;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLOutput;
-import java.util.*;
+import com.bankmgmt.app.entity.Account;
 
-
+@Service
 public class BankService {
 
     private List<Account> accounts = new ArrayList<>();
     private Integer currentId = 1;
 
     // TODO: Method to Create a new Account
+    public Account addAccount(Account account) {
+    	account.setId(currentId);
+    	account.setBalance(0.00);
+    	accounts.add(account);
+    	System.out.println("Account added");
+    	this.currentId=currentId+1;
+    	return account;
+    	
+    }
     
 
     // TODO: Method to Get All Accounts
-    
+    public List<Account> getAllAccounts(){
+    	return accounts;
+    }
 
     // TODO: Method to Get Account by ID
-    
+    public Account getAccountbyId(int id) {
+    	Account a = searchAccount(id);
+    	if (a==null) {
+    		System.out.println("Account not found");
+    		
+    	}
+    	
+    		return a;
+    }
 
     // TODO: Method to Deposit Money to the specified account id
     
+    public Account deposit(int id,double amount) {
+    	Account a = searchAccount(id);
+    	if (a==null) {
+    		System.out.println("Account not found");
+    	}
+    	if(amount<=0){
+    		System.out.println("Amount invalid");
+    		return null;
+    	}
+    	else {
+			/*
+			 * if (a.getBalance()==null) { a.setBalance(0.00); }
+			 */
+    	
+    	double totalBalance=((double)a.getBalance())+amount;
+    	a.setBalance(totalBalance);
+    	System.out.println("Added amount to balance");
+    	System.out.println("deposited amount is "+amount);
+    	System.out.println("existing balance is "+a.getBalance());
+    	System.out.println("total balance is "+totalBalance);
+    	return a;
+    	}
+    } 
 
     // TODO: Method to Withdraw Money from the specified account id
     
-
+    	public Account withdraw(int id,double amount) {
+    		Account a = searchAccount(id);
+        	if (a==null) {
+        		System.out.println("Account not found");
+        	}
+        	if(amount<=0 || amount>a.getBalance()){
+        		System.out.println("Amount invalid");
+        		return null;
+        	}
+        	else {
+				/*
+				 * if (a.getBalance()==null) { a.setBalance(0.00); }
+				 */
+        	double totalBalance=(a.getBalance())-amount;
+        	a.setBalance(totalBalance);
+        	System.out.println("withdrawn amount from balance");
+        	System.out.println("withdrawal amount is "+amount);
+        	System.out.println("existing balance is "+a.getBalance());
+        	System.out.println("total balance after withdrawal is "+totalBalance);
+        	return a;
+        	}
+    	}
     // TODO: Method to Transfer Money from one account to another
+    	
+    	public List<Account> transferFunds(int fromId, int toId,double amount) {
+    		List<Account> senderreceiverAccounts = new ArrayList<>();
+    		Account senderAccount = searchAccount(fromId); 
+    		Account receiverAccount = searchAccount(toId);
+    		if(senderAccount==null || receiverAccount == null) {
+    			System.out.println("sender or receiver acccount not found ");
+    		}
+    		if (amount>senderAccount.getBalance()) {
+    			System.out.println("low balance");
+    			senderreceiverAccounts.add(senderAccount);
+    			senderreceiverAccounts.add(receiverAccount);
+    			return senderreceiverAccounts;
+    		}
+    		else {
+			
+			  Account updatedSenderDetails = withdraw(fromId, amount); 
+			  Account updatedReceiverDetails = deposit(toId, amount);
+			  senderreceiverAccounts.add(updatedSenderDetails);
+	    	  senderreceiverAccounts.add(updatedReceiverDetails);
+    		}
+    		
+    	return senderreceiverAccounts;
+    	}
     
     
     // TODO: Method to Delete Account given a account id
-
+    public Account deleteAccount(int id) {
+    	Account a = searchAccount(id);
+    	if (a==null)
+    	{
+    		System.out.println("Account not found");
+    	}
+    	accounts.remove(a);
+    	System.out.println("Account "+a.getId()+" deleted");
+    	return null;
+    }
+    
+    
+    public Account searchAccount(int id) {
+    	for (Account ele:accounts) {
+    		if (ele.getId()==id) {
+    			return ele;
+    		}
+    		
+    		
+    	}
+    	return null;
+    }
 
 }
